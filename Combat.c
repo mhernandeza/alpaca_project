0fc37b908f06e361d212ef98806c7839e424c515
Daniel <dantan20114@gmail.com>
2015-11-26 13:12:29 +0000

    updates combat files form dan

--- Combat.c
+++ Combat.c
@@ -3,18 +3,19 @@
 #include <stdlib.h>
 #include <time.h>
 #include "encounter.h"
-#include <time.c>
+#include <time.h>
 
 
 #define BOOL int
 #define TRUE 1
 #define FALSE 0
-#define TURNTIME 750
+#define TURNTIME 1000
 
 int retreat_condiition (void);
 int AL_damageHandle (int WeaponNum, int weapondamage);
 int AL_reloadCannons (int Weapon_Number);
 int AL_shoot_cannons (int Player_charge);
+int tick (void);
 
 struct user_fake {
   int Hull;
@@ -28,10 +29,9 @@ struct user_fake {
   int gold;
 };
 
-int main2(void);
-
 int main (void)
 {
+  struct player1, player2, test;
   main2(); /* when game controller ready, delete this and replace with srand(time(NULL)) at the highest level; */
   Encounter player2; /* move this to game controller module */
   AL_getEncounter(&test); /* move this to game controller module then pass yourself a pointer to the struct */
@@ -39,41 +39,42 @@ int main (void)
   test.name,test.description,test.locale.direction,test.pronoun,test.weaponnumber,test.weapontype,test.locale.weatherdescription);
   int Player_charge = 0, Enemy_charge = 0, retreat_Counter = 10, playerfire = 0, enemyfire = 0;
   if (tick() == 1){
-    if ((AL_getRetreatHealth(player) < AL_getHealth(player) )||
-    ((AL_getRetreatWeapons(player) == 1) &&
-    (AL_getEnemyRetreatWeapon(player2) > AL_getWeaponNumber(player))) {
-      retreat_counter++;
-      printf("%d turns till escape\n", 10 - retreat_counter);
+    if ((AL_getRetreatHealth(player1) < AL_getHealth(player1) )||
+    ((AL_getRetreatWeapons(player1) == 1) &&
+    (AL_getEnemyRetreatWeapon(player2) > AL_getWeaponNumber(player1))) {
+      retreat_Counter++;
+      printf("%d turns till escape\n", 10 - retreat_Counter);
     }
-    Player_charge += AL_reloadCannons(AL_getWeaponNumber(player));
+    Player_charge += AL_reloadCannons(AL_getWeaponNumber(player1));
     Enemy_charge += AL_reloadCannons(player2.weaponnumber);
-    if (retreat_counter >= 0) {
+    if (retreat_Counter >= 0) {
       printf("Retreating!\n");
     }
-    else {
-      if (AL_shoot_cannons(Player_charge) == 1){
-        Player_charge -= 100;
-        player2.health -= AL_damageHandle(AL_getWeaponNumber(player), AL_getWeaponDamage(player));
-      }
-      if (AL_shoot_cannons(Enemy_charge) == 1){
-        AL_decreaseHealth(damageHandle(player2.weaponnumber, player2.weapondamage));
-        Enemy_charge -= 100;
-      }
+    if (AL_shoot_cannons(Player_charge) == 1){
+      Player_charge -= 100;
+      player2.health -= AL_damageHandle(AL_getWeaponNumber(player1), AL_getWeaponDamage(player1));
     }
+    if (AL_shoot_cannons(Enemy_charge) == 1){
+      AL_decreaseHealth(damageHandle(player2.weaponnumber, player2.weapondamage));
+      Enemy_charge -= 100;
+    }
+  if (player2.Hull <= 0) {
+    return 1;
   }
-  if (player2.Hull <= 0 || AL_getHealth(player) <= 0) {
+  if (AL_getHealth(player1) <= 0) {
     return 0;
   }
-  if (retreat_counter == 10){
+  if (retreat_Counter == 10){
     retreat_condiition();
-    return 0;
+    return 1;
   }
-  return 0;
+  }
+  return 1;
 }
 
 int AL_reloadCannons (int Weapon_Number)
 {
-  return (AL_getWeaponNumber(player));
+  return (AL_getWeaponNumber(player1));
 }
 
 int AL_shoot_cannons (int Player_charge)
@@ -99,8 +100,8 @@ int AL_shoot_cannons (int Player_charge)
 int AL_damageHandle (int WeaponNum, int weapondamage)
 {
   int x, y = 0, hits = 0, critical = 0, miss = 0;
-  for (x = 0 ; x < WeaponNumber ; x++) {
-    y = rand()%6
+  for (x = 0 ; x < WeaponNum ; x++) {
+    y = rand()%6;
     if (y <= 1) {
       miss++;
     }
@@ -112,9 +113,8 @@ int AL_damageHandle (int WeaponNum, int weapondamage)
       critical++;
     }
   }
-  printf("%d Shots hit the target, %d critical hits were dealt,
-  %d Damage Dealt\n%d shots missed\n", hits, critical, hits*weapondamage, miss);
-  return hits*weaponDamage
+  printf("%d Shots hit the target, %d critical hits were dealt, %d Damage Dealt\n%d shots missed\n", hits, critical, (hits*weapondamage), miss);
+  return hits*weaponDamage;
 }
 
 int retreat_condiition (void)
@@ -125,7 +125,7 @@ int retreat_condiition (void)
 
 int tick (void)
 {
-  if (time()%TURNTIME == 0){
+  if (time(NULL)%TURNTIME == 0){
     return 1;
   }
   return 0;
